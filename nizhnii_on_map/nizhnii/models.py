ddff6b0509142b725d45bc01aa5b8a621b9dc789
Ilya <ilya@MacBook-Air-Ilya.local>
2024-04-23 20:01:06 +0300

    Create model

--- nizhnii_on_map/nizhnii/models.py
+++ nizhnii_on_map/nizhnii/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class InterestingPlacesModel(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Наименование')
+    description = models.TextField(blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
